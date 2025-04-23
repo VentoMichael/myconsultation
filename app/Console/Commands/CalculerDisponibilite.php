8def22e3ec4b74cbd1bfca97fb939d4e76b06690
ventomichael <vento.michael0705@hotmail.com>
2025-04-23 20:48:49 +0200

    Ajout des dates filtrages

--- app/Console/Commands/CalculerDisponibilite.php
+++ app/Console/Commands/CalculerDisponibilite.php
@@ -11,7 +11,7 @@ class CalculerDisponibilite extends Command
      *
      * @var string
      */
-    protected $signature = 'app:calculer-disponibilite {doctor_name?}';
+    protected $signature = 'app:calculer-disponibilite {doctor_name?} {start_date?} {end_date?}';
 
     /**
      * The console command description.
@@ -28,30 +28,49 @@ class CalculerDisponibilite extends Command
         $adresseId = 1;
         $motifId = 2;
 
-        // php artisan app:calculer-disponibilite "Dr Clinique Est"
+        // php artisan app:calculer-disponibilite "Dr Cabinet Central" 2025-04-25 2025-04-28
 
         $doctorName = $this->argument('doctor_name');
+        $startDate = $this->argument('start_date') ? \Carbon\Carbon::parse($this->argument('start_date')) : null;
+        $endDate = $this->argument('end_date') ? \Carbon\Carbon::parse($this->argument('end_date')) : null;
         $service = app(\App\Services\AvailabilityService::class);
 
-        if ($doctorName) {
-            $dispo = $service->getNextDisponibilite($adresseId, $motifId);
-            $this->info('Prochaine disponibilité : ' . ($dispo ? $dispo->format('Y-m-d H:i') : 'Aucune trouvée'));
-        } else {
-            $doctors = \App\Models\User::all();
-            $motifs = \App\Models\Motif::all();
-            $addresses = \App\Models\Address::all();
-            foreach ($doctors as $doctor) {
-                foreach ($addresses as $address) {
-                    foreach ($motifs as $motif) {
-                        $dispo = $service->getNextDisponibilite($address->id, $motif->id);
-                        if ($dispo) {
-                            $this->info("Prochaine disponibilité : {$dispo->format('Y-m-d H:i')} | {$doctor->name} | {$address->name} | {$motif->name}");
-                            return;
+        $results = [];
+        $doctors = $doctorName ? \App\Models\User::where('name', $doctorName)->get() : \App\Models\User::all();
+        $motifs = \App\Models\Motif::all();
+        $addresses = \App\Models\Address::all();
+        foreach ($doctors as $doctor) {
+            foreach ($addresses as $address) {
+                foreach ($motifs as $motif) {
+                    $dispo = $service->getNextDisponibilite($address->id, $motif->id, $startDate, $endDate);
+                    if ($dispo) {
+                        if ($startDate && $endDate) {
+                            if ($dispo->between($startDate, $endDate)) {
+                                $results[] = [
+                                    'datetime' => $dispo->format('Y-m-d H:i'),
+                                    'doctor' => $doctor->name,
+                                    'address' => $address->name,
+                                    'motif' => $motif->name
+                                ];
+                            }
+                        } else {
+                            $results[] = [
+                                'datetime' => $dispo->format('Y-m-d H:i'),
+                                'doctor' => $doctor->name,
+                                'address' => $address->name,
+                                'motif' => $motif->name
+                            ];
                         }
                     }
                 }
             }
+        }
+        if (empty($results)) {
             $this->info('Aucune disponibilité trouvée');
+        } else {
+            foreach ($results as $r) {
+                $this->line("{$r['datetime']} | {$r['doctor']} | {$r['address']} | {$r['motif']}");
+            }
         }
     }
 }
